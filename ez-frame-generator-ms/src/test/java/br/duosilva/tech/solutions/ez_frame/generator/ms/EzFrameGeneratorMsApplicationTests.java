9e9aced8fc7f738c7cf9fd96edad78bbd668da8e
Thaynara Silva <thaynara.silva@yaman.com.br>
2025-03-22 18:23:14 -0300

    estrutura base do projeto

---
+++ ez-frame-generator-ms/src/test/java/br/duosilva/tech/solutions/ez_frame/generator/ms/EzFrameGeneratorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.duosilva.tech.solutions.ez_frame.generator.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzFrameGeneratorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
