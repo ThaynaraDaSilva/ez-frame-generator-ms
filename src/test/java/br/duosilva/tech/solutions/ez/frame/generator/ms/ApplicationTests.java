7993cc556b8f21e218d9c23eec1d032af924d3a7
Thaynara Silva <thaynara.silva@yaman.com.br>
2025-04-18 18:39:45 -0300

    testes no appplication.java desabilitados

--- src/test/java/br/duosilva/tech/solutions/ez/frame/generator/ms/ApplicationTests.java
+++ src/test/java/br/duosilva/tech/solutions/ez/frame/generator/ms/ApplicationTests.java
@@ -6,13 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/*
+	 * @Test void contextLoads() { }
+	 */
 
-	@Test
-	void testMainMethod() {
-		Application.main(new String[]{});
-	}
+	/*
+	 * @Test void testMainMethod() { Application.main(new String[]{}); }
+	 */
 
 }
