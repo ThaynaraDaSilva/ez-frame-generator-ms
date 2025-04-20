37ac7cb6903c3e4a2f82ae6acde528ccfed4cd93
Thaynara Silva <thaynara.silva@yaman.com.br>
2025-04-20 18:07:31 -0300

    ajuste no método de deletemessages

--- src/main/java/br/duosilva/tech/solutions/ez/frame/generator/ms/adapters/in/listener/SQSListener.java
+++ src/main/java/br/duosilva/tech/solutions/ez/frame/generator/ms/adapters/in/listener/SQSListener.java
@@ -51,7 +51,7 @@ public class SQSListener {
 	@Scheduled(fixedRate = 10000) // 5000 Runs every 5 seconds
 	public void pollMessagesFromQueue() {
 		// Fetch up to 5 messages at a time
-		ReceiveMessageRequest request = ReceiveMessageRequest.builder().queueUrl(obtainSQSUrl()).maxNumberOfMessages(10) // time
+		ReceiveMessageRequest request = ReceiveMessageRequest.builder().queueUrl(obtainSQSUrl()).maxNumberOfMessages(5) // time
 				.build();
 
 		ReceiveMessageResponse response = sqsAsyncClient.receiveMessage(request).join();
@@ -61,7 +61,6 @@ public class SQSListener {
 		} else {
 			for (Message message : response.messages()) {
 				CompletableFuture.runAsync(() -> initiateGenerateFramesProcess(message.body(), message.receiptHandle()), executorService);
-				//retrieveVideoData(message.body(), message.receiptHandle());
 			}
 		}
 	}
@@ -88,7 +87,7 @@ public class SQSListener {
 		DeleteMessageRequest deleteMessageRequest = DeleteMessageRequest.builder().queueUrl(obtainSQSUrl())
 				.receiptHandle(receiptHandle).build();
 
-		sqsAsyncClient.deleteMessage(deleteMessageRequest);
+		sqsAsyncClient.deleteMessage(deleteMessageRequest).join();
 		LOGGER.info("#### MESSAGE DELETED: {} ####", deleteMessageRequest);
 		LOGGER.info("############################################################");
 	}
